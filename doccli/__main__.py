0f4b0029d672a48bacb9330512596e291005bc29
Bastien <neitsabast@gmail.com>
2024-10-02 22:15:15 +0200

    feat(search) : Affiche le contenu d'un fichier ou d'un dossier

--- doccli/__main__.py
+++ doccli/__main__.py
@@ -1,9 +1,19 @@
+from pathlib import Path
 import typer
+import command.search as com_search
 
+app = typer.Typer()
 
+@app.command()
 def main():
     print("Hello from doccli")
 
+@app.command()
+def search(search_input: str) -> None:
+    default_dir = r"C:/Users/Bastien"
+    search_path = Path(default_dir + search_input)
+    com_search.is_file_or_dir(search_path)
+
 
 if __name__ == "__main__":
-    typer.run(main)
+    app()
--- doccli/command/search.py
+++ doccli/command/search.py
@@ -1,23 +1,64 @@
-import os
+from pathlib import Path
 
-def search(entrance_term: str, search_term: str):
-    if os.path.isdir(entrance_term):
-        try:
-            files = os.listdir(entrance_term)
-            for file in files:
-                print(file)
-        except PermissionError:
-            print("Permission denied: Unable to access some directories.")
-        except FileNotFoundError:
-            print("The directory does not exist.")
-        except Exception as e:
-            print(f"An error occurred: {e}")
-    elif os.path.isfile(entrance_term):
-        try:
-            with open(entrance_term, 'r') as file:
-                content = file.read()
-                print(content)
-        except Exception as e:
-            print(f"An error occurred while reading the file: {e}")
+def is_file_or_dir(search: Path) -> None:
+    """
+    Check whether the search input is a file or a directory.
+
+    :param search: The file or directory searched by the user.
+    :type search: Path
+    :return: None, print the result.
+
+    :Example:
+    >>> is_file_or_dir("/path/to/file/hello.txt")
+    "Hello from my file !"
+    """
+
+    # Check the path exists
+    if not search.exists():
+        print(f"{search} does not exist.")
+        return None
+
+    # Check if the path is a file or a dir
+    if search.is_dir():
+        return print_tree_dir(search)
     else:
-        print(f"{entrance_term} does not exist.")
+        return print_file_content(search)
+
+
+def print_tree_dir(search: Path) -> None:
+    for line in tree(search):
+        print(line)
+    return None
+
+def print_file_content(search: Path) -> None:
+    print("Content from : " + str(search))
+    print("- * - * - * - * - * - * - * - * -")
+    with search.open() as f:
+        for line in f:
+            print("| " + line, end='')
+    print("\n- * - * - * - * - * - * - * - * -")
+    return None
+
+
+# https://stackoverflow.com/questions/9727673/list-directory-tree-structure-in-python
+def tree(dir_path: Path, prefix: str=''):
+    # prefix components:
+    space =  '    '
+    branch = '│   '
+    # pointers:
+    tee =    '├── '
+    last =   '└── '
+
+    """A recursive generator, given a directory Path object
+    will yield a visual tree structure line by line
+    with each line prefixed by the same characters
+    """    
+    contents = list(dir_path.iterdir())
+    # contents each get pointers that are ├── with a final └── :
+    pointers = [tee] * (len(contents) - 1) + [last]
+    for pointer, path in zip(pointers, contents):
+        yield prefix + pointer + path.name
+        if path.is_dir(): # extend the prefix and recurse:
+            extension = branch if pointer == tee else space 
+            # i.e. space because last, └── , above so no more |
+            yield from tree(path, prefix=prefix+extension)
